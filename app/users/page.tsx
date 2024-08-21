d94573c0d03c8926ff101b59af10cf4c858b31e8
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-21 17:04:39 +0600

    routing url params

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
   return (
     <>
       <h1>Users</h1>
-      <p>{new Date().toLocaleDateString()}</p>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+      <UserTable />
     </>
   );
 };
