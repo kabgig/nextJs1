c59dac9a0ad94b674625dd260852c51cd978017a
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-22 18:27:51 +0600

    zod validation

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "../schema";
 
 export function GET(
   request: NextRequest,
@@ -14,9 +15,21 @@ export async function PUT(
   { params }: { params: { id: number } }
 ) {
   const body = await request.json();
-  if (!body.name)
-    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  const validation = schema.safeParse(body);
+
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
   if (params.id > 10)
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  if (params.id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  return NextResponse.json({ message: "User deleted" });
 }
