ec5f581156a1e5b48b140bc2d1a89796190b689b
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-23 18:32:46 +0600

    added images

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import trees from "@/public/images/1.jpg";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="Trees"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={75}
+      ></Image>
     </main>
   );
 }
