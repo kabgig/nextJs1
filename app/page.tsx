481b67dfdce28a6697087265e8e1f415a619663b
Ansar Ismagilov <119264390+kabgig@users.noreply.github.com>
2024-08-24 13:18:51 +0600

    dynamic lazy import

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,19 @@
-import { Metadata } from "next";
+"use client";
 
-export default async function Home() {
+export default function Home() {
   return (
     <main className="relative h-screen">
-      <h1 className="heading">Heading</h1>
+      <h1>Heading</h1>
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+          const users = [{ name: "b" }, { name: "c" }, { name: "a" }];
+          const sorted = _.sortBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-  const product = await fetch("https://api.example.com");
-  return {
-    title: "product.name",
-    description: "Description",
-  };
 }
